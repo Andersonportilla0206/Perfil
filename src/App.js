9c9135f973e2c8e11b5214190db7df7ed2748820
Andersonportilla0206 <andersonportilla0206@gmail.com>
2023-02-08 14:30:52 -0500

    subiendo repositorio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+    <h1>Hola Daniel</h1>
+    <h2>Hola hermosa </h2>
+    <h3>Hola hijo</h3>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Productos.jsx
@@ -0,0 +1,17 @@
+
+import * as React from 'react';
+type Props = {
+  
+};
+type State = {
+  
+};
+export class Productos extends React.Component<Props, State>{
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  };
+};
